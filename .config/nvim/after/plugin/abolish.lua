17efdc3704a4bb1426930e1b4952edbfa84d862b
Rok Mušič <rokmusic0@gmail.com>
2024-11-28 07:35:59 +0100

    deleted unused files
    I think some of them had been moved before

--- .config/nvim/after/plugin/abolish.lua
+++
@@ -1,2 +0,0 @@
--- slovenian words, or substitute č, š, ž
-vim.cmd([[:Abolish vprasanj{e,a,u,em,i,om} vprašanj{}]])
--- .config/nvim/lua/rok/custom/init.lua
+++
@@ -1,2 +0,0 @@
-require("rok.aaaaaaaaaaaa.edited_buffers")
-require("rok.aaaaaaaaaaaa.load_lazy_plugin")
