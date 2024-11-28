96d58cbe235ec166a2b4fcd47232f99857baddcb
Rok Mušič <rokmusic0@gmail.com>
2024-11-28 07:34:07 +0100

    trouble add which-key desc and removed ]q mappings
    they are defined in repeatable-move

--- .config/nvim/lua/rok/plugins/trouble.lua
+++ .config/nvim/lua/rok/plugins/trouble.lua
@@ -8,46 +8,17 @@ return {
             open_folds = { "zR", "zr", "<cr>" },
         },
     },
-
     keys = {
+        { "<leader>ll", "<cmd>Trouble<cr>", desc = ":Trouble" },
         { "<leader>ld", "<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>", desc = "[l]ist [d]iagnostics" },
         { "<leader>lD", "<cmd>Trouble diagnostics toggle focus=true<cr>", desc = "[l]ist workspace [D]iagnostics" },
         { "<leader>ls", "<cmd>Trouble symbols toggle focus=false win.position=left<cr>", desc = "[l]ist [s]ymbols" },
-        {
-            "<leader>lL",
-            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "[l]ist [L]SP info",
-        },
-        { "<leader>ll", "<cmd>Trouble loclist toggle focus=true<cr>", desc = "[l]ist [l]oclist" },
         { "<leader>lq", "<cmd>Trouble qflist toggle focus=true<cr>", desc = "[l]ist [q]uickfix" },
         { "<leader>lc", "<cmd>Trouble todo toggle focus=true<cr>", desc = "[l]ist [c]omments" },
-        {
-            "[q",
-            function()
-                if require("trouble").is_open() then
-                    require("trouble").prev({ skip_groups = true, jump = true })
-                else
-                    local ok, err = pcall(vim.cmd.cprev)
-                    if not ok then
-                        vim.notify(err, vim.log.levels.ERROR)
-                    end
-                end
-            end,
-            desc = "Previous Quickfix Item",
-        },
-        {
-            "]q",
-            function()
-                if require("trouble").is_open() then
-                    require("trouble").next({ skip_groups = true, jump = true })
-                else
-                    local ok, err = pcall(vim.cmd.cnext)
-                    if not ok then
-                        vim.notify(err, vim.log.levels.ERROR)
-                    end
-                end
-            end,
-            desc = "Next Quickfix Item",
-        },
     },
+    config = function(_, opts)
+        require("trouble").setup(opts)
+
+        require("which-key").add({ { "<leader>l", group = "[l]og" } })
+    end,
 }
