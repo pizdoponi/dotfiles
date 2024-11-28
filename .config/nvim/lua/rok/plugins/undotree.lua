b8e7239c33868747d45dbb1772c198b209a6adf3
Rok Mušič <rokmusic0@gmail.com>
2024-11-28 07:33:06 +0100

    undotree change layout and bind <down>, <up>

--- .config/nvim/lua/rok/plugins/undotree.lua
+++ .config/nvim/lua/rok/plugins/undotree.lua
@@ -1,13 +1,21 @@
 return {
     "mbbill/undotree",
-    cmd = { "UndotreeToggle" },
+    cmd = { "UndotreeToggle", "UndotreePersistUndo" },
     keys = {
         { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "[u]ndotree" },
     },
     config = function()
+        vim.g.undotree_WindowLayout = 2
         vim.g.undotree_SplitWidth = 40
-        vim.g.undotree_DiffpanelHeight = 12
+        vim.g.undotree_DiffpanelHeight = 10
         vim.g.undotree_DiffAutoOpen = 1
         vim.g.undotree_SetFocusWhenToggle = 1
+
+        vim.cmd([[
+            function g:Undotree_CustomMap()
+                nmap <buffer> <up> <plug>UndotreeNextState
+                nmap <buffer> <down> <plug>UndotreePreviousState
+            endfunc
+        ]])
     end,
 }
