244c308104cc2f3c7c5cfea24727ad28dae5e9bc
Rok Mušič <rokmusic0@gmail.com>
2024-11-28 07:36:41 +0100

    disable copilot in env files

--- .config/nvim/lua/rok/autocmds.lua
+++ .config/nvim/lua/rok/autocmds.lua
@@ -9,6 +9,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 -- always, in every buffer, set relative line numbers
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "BufWinEnter", "TabEnter" }, {
     command = "setlocal relativenumber",
+})
+
+-- Set filetype to 'env' for .env and similar files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.env", "*.env.*" },
+    callback = function()
+        vim.bo.filetype = "dot"
+    end,
 })
