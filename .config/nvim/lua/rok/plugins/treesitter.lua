77c9bd4cbb900b53d545bc685e20c9d31e550d4c
Rok Mušič <rokmusic0@gmail.com>
2024-12-20 10:39:53 +0100

    feat(nvim): add Treewalker

--- .config/nvim/lua/rok/plugins/treesitter.lua
+++ .config/nvim/lua/rok/plugins/treesitter.lua
@@ -125,14 +125,6 @@ return {
             vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
             vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
             vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
-
-            -- Register which-key mappings
-            pcall(function()
-                require("which-key").add({
-                    { "<leader>;", group = "Swap next" },
-                    { "<leader>,", group = "Swap prev" },
-                })
-            end)
         end,
     },
     {
@@ -149,5 +141,13 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         opts = { useDefaultKeymaps = true },
+    },
+    {
+        "aaronik/treewalker.nvim",
+        lazy = true,
+        cmd = { "Treewalker" },
+        opts = {
+            highlight = false,
+        },
     },
 }
