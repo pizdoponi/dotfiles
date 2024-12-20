0137ec511cc9cc57caf76efea5f50ef97a38e491
Rok Mušič <rokmusic0@gmail.com>
2024-12-20 09:47:05 +0100

    feat(nvim): add hydra

---
+++ .config/nvim/lua/rok/plugins/hydra.lua
@@ -0,0 +1,30 @@
+return {
+    "anuvyklack/hydra.nvim",
+    config = function()
+        local Hydra = require("hydra")
+
+        Hydra({
+            name = "wincmd",
+            mode = "n",
+            body = "<C-w>",
+            heads = {
+                { ">", "<C-w>>", { desc = "Increase Width" } },
+                { "<", "<C-w><", { desc = "Decrease Width" } },
+                { "+", "<C-w>+", { desc = "Increase Height" } },
+                { "-", "<C-w>-", { desc = "Decrease Height" } },
+            },
+        })
+
+        Hydra({
+            name = "Treewalker",
+            mode = "n",
+            body = "<leader>T",
+            heads = {
+                { "<down>", "<cmd>Treewalker Down<cr>", { desc = "Move Down" } },
+                { "<up>", "<cmd>Treewalker Up<cr>", { desc = "Move Up" } },
+                { "<left>", "<cmd>Treewalker Left<cr>", { desc = "Move Left" } },
+                { "<right>", "<cmd>Treewalker Right<cr>", { desc = "Move Right" } },
+            },
+        })
+    end,
+}
