287f80be58f34a1741f685310f6da1f911f297fb
Rok Mušič <rokmusic0@gmail.com>
2024-12-20 09:53:42 +0100

    fix(nvim): change smear-cursor settings

--- .config/nvim/lua/rok/plugins/smooth_smooth_fruit.lua
+++ .config/nvim/lua/rok/plugins/smooth_smooth_fruit.lua
@@ -8,12 +8,17 @@ return {
     {
         "sphamba/smear-cursor.nvim",
         -- TODO: enable this once it gets stable. keeps craching atm.
-        enabled = false,
+        enabled = true,
         opts = {
-            stiffness = 0.7,
-            trailing_stiffness = 0.4,
-            trailing_exponent = 0.1,
-            distance_stop_animating = 0.1,
+            stiffness = 0.8, -- 0.6      [0, 1]
+            trailing_stiffness = 0.6, -- 0.3      [0, 1]
+            trailing_exponent = 0, -- 0.1      >= 0
+            distance_stop_animating = 0.5, -- 0.1      > 0
+            hide_target_hack = false, -- true     boolean
+            -- stiffness = 0.7,
+            -- trailing_stiffness = 0.4,
+            -- trailing_exponent = 0.1,
+            -- distance_stop_animating = 0.1,
         },
     },
 }
