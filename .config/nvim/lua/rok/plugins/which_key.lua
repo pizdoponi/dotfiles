927681a7cf0d273b7a9c2c039ea518f3a2c23ed9
Rok Mušič <rokmusic0@gmail.com>
2024-12-20 09:55:37 +0100

    feat(nvim): add keymap group descriptions in which-key

--- .config/nvim/lua/rok/plugins/which_key.lua
+++ .config/nvim/lua/rok/plugins/which_key.lua
@@ -25,6 +25,18 @@ return {
             { "<leader>w", group = "[w]indow" },
             { "<leader>b", group = "[b]uffer" },
             { "<leader>g", group = "[g]it" },
+            { "<leader>gd", group = "[g]it [d]iff" },
+            { "<leader>gt", group = "[g]it [t]oggle" },
+            { "<leader>l", group = "[l]og" },
+            { "<leader>t", group = "[t]erm/[t]est" },
+            { "<leader>s", group = "[s]earch" },
+            { "<leader>sn", group = "[s]earch [n]eovim" },
+            { "<leader>d", group = "[d]ebug" },
+            { "<leader>dl", group = "[d]ebug [l]og" },
+            { "<leader>r", group = "[r]epl" },
+            { "<leader>T", group = "[T]reewalker" },
+            { "<leader>;", group = "Swap next" },
+            { "<leader>,", group = "Swap prev" },
         })
     end,
 }
