a1e75fd4fb2c3f9daa352dad98114a79796a1fdc
Rok Mušič <rokmusic0@gmail.com>
2024-12-20 09:44:44 +0100

    fix(nvim): update repeatable move for qflist (trouble)

--- .config/nvim/lua/rok/repeatable_move.lua
+++ .config/nvim/lua/rok/repeatable_move.lua
@@ -15,25 +15,21 @@ local goto_prev_spell = function()
     vim.cmd("normal! [s")
 end
 
-Goto_prev_qflist = function()
-    if require("trouble").is_open() then
-        require("trouble").prev({ skip_groups = true, jump = true })
+local goto_next_qflist = function()
+    local trouble_ok, trouble = pcall(require("trouble"))
+    if trouble_ok and trouble.is_open() then
+        trouble.next({ skip_groups = true, jump = true })
     else
-        local ok, err = pcall(vim.cmd.cprev)
-        if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-        end
+        vim.cmd("cnext")
     end
 end
 
-Goto_next_qflist = function()
-    if require("trouble").is_open() then
-        require("trouble").next({ skip_groups = true, jump = true })
+local goto_prev_qflist = function()
+    local trouble_ok, trouble = pcall(require("trouble"))
+    if trouble_ok and trouble.is_open() then
+        trouble.prev({ skip_groups = true, jump = true })
     else
-        local ok, err = pcall(vim.cmd.cnext)
-        if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-        end
+        vim.cmd("cprev")
     end
 end
 
@@ -41,11 +37,15 @@ local repeat_goto_next_diagnostic, repeat_goto_prev_diagnostic =
     repeatable_move(vim.diagnostic.goto_next, vim.diagnostic.goto_prev)
 local repeat_goto_next_error, repeat_goto_prev_error = repeatable_move(goto_next_error, goto_prev_error)
 local repeat_goto_next_spell, repeat_goto_prev_spell = repeatable_move(goto_next_spell, goto_prev_spell)
+local repeat_goto_next_qflist, repeat_goto_prev_qflist = repeatable_move(goto_next_qflist, goto_prev_qflist)
 
 vim.keymap.set({ "n", "x", "o" }, "]d", repeat_goto_next_diagnostic, { desc = "Next diagnostic" })
 vim.keymap.set({ "n", "x", "o" }, "[d", repeat_goto_prev_diagnostic, { desc = "Prev diagnostic" })
 vim.keymap.set({ "n", "x", "o" }, "]e", repeat_goto_next_error, { desc = "Next error" })
 vim.keymap.set({ "n", "x", "o" }, "[e", repeat_goto_prev_error, { desc = "Prev error" })
+
+vim.keymap.set({ "n", "x", "o" }, "]q", repeat_goto_next_qflist, { desc = "Next qflist" })
+vim.keymap.set({ "n", "x", "o" }, "[q", repeat_goto_prev_qflist, { desc = "Prev qflist" })
 
 vim.keymap.set({ "n", "x", "o" }, "]s", repeat_goto_next_spell, { desc = "Next spell" })
 vim.keymap.set({ "n", "x", "o" }, "[s", repeat_goto_prev_spell, { desc = "Prev spell" })
