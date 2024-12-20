f76aaf7bf30d9b85baeb81f98380f0b21eaf48c8
Rok Mušič <rokmusic0@gmail.com>
2024-12-20 10:10:10 +0100

    fix(nvim): remove autocmd for help files

--- .config/nvim/lua/rok/autocmds.lua
+++ .config/nvim/lua/rok/autocmds.lua
@@ -1,13 +1,3 @@
--- open :help pages in vsplit on the right
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*.txt",
-    callback = function()
-        if vim.bo.buftype == "help" then
-            vim.cmd("wincmd L")
-        end
-    end,
-})
-
 -- always, in every buffer, set relative line numbers
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "BufWinEnter", "TabEnter" }, {
     command = "setlocal relativenumber",
