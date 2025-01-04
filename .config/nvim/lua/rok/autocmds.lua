19946313f96610d9174f187fb00a68cac9bf3336
Rok Mušič <rokmusic0@gmail.com>
2025-01-04 10:22:23 +0100

    feat(nvim): set fileencoding to utf-8 if no set

--- .config/nvim/lua/rok/autocmds.lua
+++ .config/nvim/lua/rok/autocmds.lua
@@ -1,12 +1,26 @@
 -- always, in every buffer, set relative line numbers
-vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "BufWinEnter", "TabEnter" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "TabEnter" }, {
     command = "setlocal relativenumber",
 })
 
 -- Set filetype to 'env' for .env and similar files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
     pattern = { "*.env", "*.env.*" },
     callback = function()
         vim.bo.filetype = "dot"
+        print("Setting filetype to dot")
+    end,
+})
+
+-- After loading a buffer, if the fileencoding is not set, set it to utf-8
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+    pattern = "*",
+    callback = function()
+        if vim.bo.filetype == "qf" then
+            return
+        end
+        if vim.bo.fileencoding == "" then
+            vim.bo.fileencoding = "utf-8"
+        end
     end,
 })
