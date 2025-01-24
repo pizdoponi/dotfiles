5ebbecb1dff12bd2767b6c453ec7e29b7dad4e79
Rok Mušič <rokmusic0@gmail.com>
2025-01-24 15:18:05 +0100

    fix(nvim): add augroup and desc to autocmds

--- .config/nvim/lua/rok/autocmds.lua
+++ .config/nvim/lua/rok/autocmds.lua
@@ -1,19 +1,24 @@
--- always, in every buffer, set relative line numbers
+local group = vim.api.nvim_create_augroup("pizdoponi", { clear = false })
+
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "TabEnter" }, {
-    command = "setlocal relativenumber",
+    group = group,
+    desc = "Set number and relativenumber in every buffer",
+    command = "setlocal number relativenumber",
 })
 
--- Set filetype to 'env' for .env and similar files
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+    group = group,
+    desc = "Set filetype to dot for .env files",
     pattern = { "*.env", "*.env.*" },
     callback = function()
         vim.bo.filetype = "dot"
-        print("Setting filetype to dot")
+        vim.notify("Setting filetype to dot", vim.log.levels.INFO)
     end,
 })
 
--- After loading a buffer, if the fileencoding is not set, set it to utf-8
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+    group = group,
+    desc = "Set fileencoding to utf-8 if not set",
     pattern = "*",
     callback = function()
         if vim.bo.filetype == "qf" then
